1f5e7d319a1347eb638861b55d07847619ac5f4a
watasuke102 <watasuke102@gmail.com>
2021-12-01 00:19:51 +0900

    update: receive notice when someone entered to voice channel

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,40 @@
 use serde::{Deserialize, Serialize};
+use serenity::framework::StandardFramework;
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready, id::GuildId, voice::VoiceState},
+    prelude::*,
+};
 use std::fs::File;
 use std::io::BufReader;
+use std::option::Option;
 
+struct Handler;
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, _ctx: Context, _new_message: Message) {
+        println!("{:?}", _new_message);
+    }
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!(
+            "Info: connected {} [version: {}]",
+            ready.user.name, ready.version
+        );
+    }
+    async fn voice_state_update(
+        &self,
+        _ctx: Context,
+        guild_id: Option<GuildId>,
+        _: Option<VoiceState>,
+        new: VoiceState,
+    ) {
+        println!("*********************");
+        println!("guild_id: {:?}", guild_id);
+        if let Some(n) = &new.member {
+            println!("[new] {:#?}", n.user);
+        }
+    }
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Settings {
@@ -10,21 +43,29 @@ struct Settings {
     log_channel_id: String,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let file = File::open("env.json")
         .expect("cannot read `env.json`: did you create this file? try `cp sample-env.json env.json` and edit it.");
     let settings: Settings = serde_json::from_reader(BufReader::new(file)).unwrap();
     {
         let empty = String::new();
         if settings.discord_token == empty {
-            panic!("FATAL: `discord_token` in env.json is empty");
+            panic!("ERROR: `discord_token` in env.json is empty");
         }
         if settings.voice_channel_id == empty {
-            panic!("FATAL: `voice_id` in env.json is empty");
+            panic!("ERROR: `voice_id` in env.json is empty");
         }
         if settings.log_channel_id == empty {
-            panic!("FATAL: `log_channel_id` in env.json is empty");
+            panic!("ERROR: `log_channel_id` in env.json is empty");
         }
     }
-    println!("{:?}", settings);
+    let mut client = Client::builder(&settings.discord_token)
+        .event_handler(Handler)
+        .framework(StandardFramework::new())
+        .await
+        .expect("ERROR: failed to create client");
+    if let Err(why) = client.start().await {
+        println!("ERROR: client error: {:#?}", why);
+    }
 }
